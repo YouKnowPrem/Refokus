c542115beb3b51c123e96337eabe3a70faad0e38
Prem Singh <thepremsingh2111@gmail.com>
2024-09-14 21:10:31 +0530

    Update ContactPage.tsx

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -9,8 +9,7 @@ function ContactForm() {
           <h1 className='text-[5vw] uppercase leading-none'>Don't Be Shy with us</h1>
           <div>
             <p className='w-1/2 text-[.8vw] tracking-tight'>
-              Don’t like forms? Well, too bad! This one is connected to our CRM and makes everything 
-              easier. Ok ok... here we go:
+              Don't like forms? Well, too bad! Ok ok... here we go:
             </p>
             <button 
               className='text-[3vw] sm:text-[.8vw]
@@ -18,13 +17,13 @@ function ContactForm() {
               sm:py-[.5vw] rounded-full sm:mt-[1.5vw] 
               border-[1px] border-secondary'
             >
-              hello@refokus.com  
+              thepremsingh2111@gmail.com 
             </button>
           </div>  
         </div>
 
         <div className='w-1/2 mt-[6vw]'>
-          <Faq /> 
+          <Faq/> 
         </div>
         {/* //form */}
         <div></div>
